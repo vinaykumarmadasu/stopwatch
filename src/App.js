a749ed5d30a679f5c62acfc72fbc82fd40ac470c
vinay kumar <madasuvinaykumar123@gmail.com>
2024-11-10 17:17:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [intervalTracker, setIntervalTracker] = useState(-1);
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ marginLeft: "20px" }}>
+      <h1>Stopwatch</h1>
+      <p>Time: {formatTime(time)}</p>
+      {intervalTracker === -1 && (
+        <button
+          onClick={() => {
+            const intervalId = setInterval(() => {
+              setTime((t) => t + 1);
+            }, 1000);
+            setIntervalTracker(intervalId);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Start
+        </button>
+      )}
+      {intervalTracker !== -1 && (
+        <button
+          onClick={() => {
+            clearInterval(intervalTracker);
+            setIntervalTracker(-1);
+          }}
+        >
+          Stop
+        </button>
+      )}
+      <button
+        onClick={() => {
+          clearInterval(intervalTracker);
+          setTime(0);
+          setIntervalTracker(-1);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
